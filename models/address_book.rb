e90e4227435cd08a41894eb56a9b819fe215a1fa
Torie <victoriaevandijk@gmail.com>
2015-12-28 10:57:17 +0000

    Checkpoint 22 Material Completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -7,6 +7,27 @@
    def initialize
      @entries = []
    end
+   
+   def binary_search(name)
+       lower = 0
+       upper = @entries.length - 1
+       
+    while lower <= upper
+       
+        mid = (lower + upper) / 2
+        mid_name = @entries[mid].name
+       
+        if name == mid_name
+           return @entries[mid]
+        elsif name < mid_name
+            upper = mid - 1
+        elsif name > mid_name
+            lower = mid + 1
+        end
+    end
+       
+       return nil
+   end
  
    def add_entry(name, phone, email)
      index = 0
