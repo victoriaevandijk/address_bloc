0683f5559892da30b922ed8240cd8bc52f5e5dd0
Torie <victoriaevandijk@gmail.com>
2016-01-01 14:08:26 +0000

    Checkpoint 22 Assignment

--- models/address_book.rb
+++ models/address_book.rb
@@ -28,6 +28,16 @@
        
        return nil
    end
+   
+   def iterative_search(name)
+       @entries.each do |entry|
+           if entry.name == name
+               return entry
+           end
+       end
+       return nil
+   end
+       
  
    def add_entry(name, phone, email)
      index = 0
